9dfdc000307b0a245a9a60fe138d20f6b85bd393
George Moschovitis <gmosx@reizu.com>
2018-04-10 10:36:00 +0300

    Added standardDeviation tests

--- Tests/StatisticsTests/DescriptiveStatistics+DispersionTests.swift
+++ Tests/StatisticsTests/DescriptiveStatistics+DispersionTests.swift
@@ -9,7 +9,15 @@ class DescriptiveStatisticsDispersionTests: XCTestCase {
         XCTAssertEqual([1.21, 3.4, 2, 4.66, 1.5, 5.61, 7.22].variance(), 5.16122, accuracy: 0.00001)
     }
 
+    func testStandardDeviation() {
+        XCTAssertEqual([].standardDeviation(), 0)
+        XCTAssertEqual([1].standardDeviation(), 0)
+        XCTAssertEqual([5, 5, 5, 5].standardDeviation(), 0)
+        XCTAssertEqual([1.21, 3.4, 2, 4.66, 1.5, 5.61, 7.22].standardDeviation(), 2.27183, accuracy: 0.00001)
+    }
+
     static var allTests = [
         ("testVariance", testVariance),
+        ("testStandardDeviation", testStandardDeviation),
     ]
 }
