0e3d8bc17887f0e6be07b1bb77acf7aaf3fadd1f
George Moschovitis <gmosx@reizu.com>
2018-05-25 19:32:34 +0300

    Initial moments code

--- Sources/Statistics/DescriptiveStatistics+Shape.swift
+++ Sources/Statistics/DescriptiveStatistics+Shape.swift
@@ -1,2 +1,47 @@
-// TODO: kurtosis
-// TODO: skewness
+import Foundation
+
+extension Collection where Element == Double {
+    /// moment = Σ[x^k] / n
+    public func moment(order: Int) -> Element {
+        assert(count > 0) // TODO: consider precondition?
+
+        if count < 1 {
+            return 0
+        }
+
+        let n = Element.init(count)
+
+        var sum: Element = 0
+
+        for x in self {
+            sum += pow(x, Double(order))
+        }
+
+        return sum / n
+    }
+
+    /// centralMoment = Σ[(x - xm)^k] / n
+    public func centralMoment(mean: Element? = nil, order: Int) -> Element {
+        assert(count > 0) // TODO: consider precondition?
+
+        if count < 1 {
+            return 0
+        }
+
+        let xm = mean ?? self.mean()
+        let n = Element.init(count)
+
+        var sum: Element = 0
+
+        for x in self {
+            let deviation = x - xm
+            sum += pow(deviation, Double(order))
+        }
+
+        return sum / n
+    }
+
+    // TODO: skewness (3rd order centralMoment)
+
+    // TODO: kurtosis (4th order centralMoment)
+}
