97466c6c18e21f7a9644c562d4d3b022d474cd5e
George Moschovitis <gmosx@reizu.com>
2018-05-25 19:48:00 +0300

    Added movingMapExpanded version and non throwing version.
    We need better naming here, any ideas?

--- Sources/Statistics/Collection+Extension.swift
+++ Sources/Statistics/Collection+Extension.swift
@@ -17,4 +17,48 @@ public extension Collection {
 
         return result
     }
+
+    // TODO: find a better name, ideas?
+    public func movingMapExpanded<T>(window windowCount: Int, fill: T, _ transform: (SubSequence) throws -> T) rethrows -> [T] {
+        // TODO: optimize
+
+        assert(windowCount > 0)
+
+        let resultCount = count
+
+        var result = [T]()
+        result.reserveCapacity(resultCount)
+
+        for i in 0..<resultCount {
+            if i < windowCount {
+                result.append(fill)
+            } else {
+                result.append(try transform(dropFirst(i).prefix(windowCount)))
+            }
+        }
+
+        return result
+    }
+
+    // TODO: find a better name, ideas?
+    public func movingMapExpanded<T>(window windowCount: Int, fill: T, _ transform: (SubSequence) -> T) -> [T] {
+        // TODO: optimize
+
+        assert(windowCount > 0)
+
+        let resultCount = count
+
+        var result = [T]()
+        result.reserveCapacity(resultCount)
+
+        for i in 0..<resultCount {
+            if i < windowCount {
+                result.append(fill)
+            } else {
+                result.append(transform(dropFirst(i).prefix(windowCount)))
+            }
+        }
+
+        return result
+    }
 }
