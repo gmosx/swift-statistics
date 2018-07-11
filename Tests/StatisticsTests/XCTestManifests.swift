81419e71105a4a1401d46f3a3f5c10caa4004863
George Moschovitis <gmosx@reizu.com>
2018-07-11 22:18:02 +0300

    Extracted some general Sequence/Collection extensions to DataProcessing library

--- Tests/StatisticsTests/XCTestManifests.swift
+++ Tests/StatisticsTests/XCTestManifests.swift
@@ -3,7 +3,6 @@ import XCTest
 #if !os(macOS)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(CollectionExtensionTests.allTests),
         testCase(DescriptiveStatisticsTests.allTests),
         testCase(DescriptiveStatisticsLocationTests.allTests),
         testCase(DescriptiveStatisticsDispersionTests.allTests),
